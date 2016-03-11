471811e8b6d3bc18322caf0812a2b6e6188377e8
vipulperiwal <vipulperiwal@Vipul.local>
2016-03-10 19:41:33 -0500

    added reverse for now

---
+++ robot01/Robot01-0307/src/org/usfirst/frc5265/Robot01/commands/ReverseBot.java
@@ -0,0 +1,63 @@
+package org.usfirst.frc5265.Robot01.commands;
+
+import org.usfirst.frc5265.Robot01.Robot;
+import edu.wpi.first.wpilibj.Joystick;
+import org.usfirst.frc5265.Robot01.commands.*;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ReverseBot extends Command {
+	boolean reverse;
+    public ReverseBot() { 
+    	/* xPos = Robot.oi.driveStick.getX();
+		double yPos = Robot.oi.driveStick.getY();
+    	reverse = Robot.ReverseStateToggle();
+    		if (reverse){
+    			xPos = -xPos;
+    			yPos = -yPos; 
+    			}else{*/	
+    			
+    			
+    			//DriveBase.tankDrive(speed, speed);
+    			//Robot.driveBase.GoForward(speed);
+    			//robotDrive.tankDrive(left, right);
+    	}
+    	
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    		reverse = Robot.ReverseStateToggle();
+        	double xPos = Robot.oi.driveStick.getX();
+    		double yPos = Robot.oi.driveStick.getY();
+        	
+        		if (reverse){
+        			xPos = -xPos;
+        			yPos = -yPos; 
+        			}
+    }
+    
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
